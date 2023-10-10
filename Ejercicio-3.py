030e3e963cb5d65df3247e6b087ad2a84168f363
RominaYanaGutierrez <145816471+RominaYanaGutierrez@users.noreply.github.com>
2023-10-09 22:29:25 -0500

    Update Ejercicio-3.py

--- Ejercicio-3.py
+++ Ejercicio-3.py
@@ -1,16 +1,23 @@
+numeros = []
+
+while True:
+    respuesta = input("¿Desea ingresar un número? (SI/NO): ")
+    
+    if respuesta.upper() == "NO":
+        break
+    
+    numero = int(input("Ingrese el número: "))
+    numeros.append(numero)
+
 nros_pares = 0
 nros_impares = 0
 
-while True:
-    nro = int(input("Ingrese un número (o ingrese 0 para salir): "))
-    
-    if nro == 0:
-        break
-    
-    if nro % 2 == 0:
+for numero in numeros:
+    if numero % 2 == 0:
         nros_pares += 1
     else:
         nros_impares += 1
 
+print("Números ingresados:", numeros)
 print("Cantidad de números pares:", nros_pares)
 print("Cantidad de números impares:", nros_impares)
