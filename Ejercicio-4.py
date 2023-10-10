c3b0379d29cb40e415d79e0e0cf82474cf3fde37
RominaYanaGutierrez <145816471+RominaYanaGutierrez@users.noreply.github.com>
2023-10-09 22:30:55 -0500

    Update Ejercicio-4.py

--- Ejercicio-4.py
+++ Ejercicio-4.py
@@ -1,1 +1,20 @@
+alumnos = []
 
+while True:
+    alumno = {}
+    alumno["Alumno"] = input("Ingrese el nombre del alumno (o escriba 'salir' para finalizar): ")
+    
+    if alumno["Alumno"].lower() == "salir":
+        break
+    
+    notas = []
+    for i in range(3):
+        nota = int(input(f"Ingrese la calificación {i+1} del alumno: "))
+        notas.append(nota)
+    
+    alumno["Notas"] = notas
+    alumnos.append(alumno)
+
+print("Listado completo de alumnos:")
+for alumno in alumnos:
+    print(f"Alumno: {alumno['Alumno']}, Notas: {alumno['Notas']}")
